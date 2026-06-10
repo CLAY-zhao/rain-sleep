6808c543589cab0a392faf7f4eb46b605898f9c4
小芳芳感冒了 <zhao1998@mm.com>
2026-06-10 15:04:55 +0800

    init

--- src/components/audios.js
+++ src/components/audios.js
@@ -1,5 +1,5 @@
 export const DEFAULT_AUDIOS = [
-    { id: 1, title: '🌧️ 轻柔小雨声', url: 'https://res.ctmus.cn/hn2-yunshang-prd1/mp3/2026/06/10/c/67/0d4c701f-9b73-489f-900d-8f2ca4d64d8e.mp3', duration: '00:15', isDefault: true },
+    { id: 1, title: '🌧️ 轻柔小雨声', url: 'https://raw.githubusercontent.com/CLAY-zhao/rain-sleep/refs/heads/main/audios/light-rain.mp3', duration: '00:15', isDefault: true },
     { id: 2, title: '🌩️ 雷雨交加', url: 'https://ma.vjshi.com/sound/2025-11-20/96679_VJshi_92c0c3b8f2234ad6adc7d73850eed19f.mp3', duration: '00:21', isDefault: true },
     { id: 3, title: '🏜️ 沙漠之夜', url: 'https://ma.vjshi.com/sound/2025-10-27/44505_VJshi_5a1ac3f89e274b16a57892ceb2c89591.mp3', duration: '00:09', isDefault: true },
     { id: 4, title: '🦗 旷野蟋蟀', url: 'https://ma.vjshi.com/sound/2025-10-27/48882_VJshi_2fb9079ddf56452d8ac5e84269bea829.mp3', duration: '00:06', isDefault: true },
